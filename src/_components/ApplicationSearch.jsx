bb8dd04609f02e943d797f17ac617ad0e02ef2bf
Lubasi Milupi <lubasimilupi@gmail.com>
2025-01-30 10:53:18 +0200

    Loaders for search and application submission

--- src/_components/ApplicationSearch.jsx
+++ src/_components/ApplicationSearch.jsx
@@ -6,10 +6,12 @@ import BackButton from "@/Helper Components/BackButton";
 import { useNavigate } from "react-router-dom";
 import axiosInstance from "@/api/axiosInstance";
 import ReCAPTCHA from "react-google-recaptcha";
+import { ClipLoader } from "react-spinners";
 
 const ApplicationSearch = () => {
   const [refNum, setRefNum] = useState("");
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
   const [isVerified, setIsVerified] = useState(false);
   const handleCaptcha = (value) => {
@@ -23,6 +25,7 @@ const ApplicationSearch = () => {
 
   async function handleClick() {
     console.log("Attempting to search for application");
+    setLoading(true);
 
     try {
       const response = await axiosInstance.get(
@@ -35,10 +38,12 @@ const ApplicationSearch = () => {
         "currentApplication",
         JSON.stringify(response.data)
       );
+      setLoading(false);
 
       //Navigate to the status page and receive the sessionStorage
       navigate(`/status/${refNum}`);
     } catch (error) {
+      setLoading(false);
       console.log(error);
     }
   }
@@ -47,56 +52,35 @@ const ApplicationSearch = () => {
     <div className="w-full px-20 py-20 bg-cecOrange h-screen">
       <div className="z-10 w-full md:w-4/5 bg-white rounded-md h-full px-10 py-10 mx-auto flex flex-col items-center justify-center gap-2">
         {isVerified ? (
-          <>
-            <h2 className="text-3xl whitespace-wrap">
-              Enter Reference Number below to check for application status
-            </h2>
-            <Input
-              type="text"
-              id=""
-              className="w-[250px] border border-neutral-500 rounded-md  focus:outline-none"
-              placeholder="REFERNCE NUMBER"
-              value={refNum}
-              onChange={(e) => {
-                setRefNum(e.target.value);
-              }}
-            />
-            <Button
-              onClick={handleClick}
-              className="bg-cecOrange text-white text-center h-[40px] w-[40px] rounded-full hover:bg-[#8C6200] mb-12"
-            >
-              <Search size={15} />
-            </Button>
-            <BackButton backTo={"/"} />
-          </>
+          loading ? (
+            <ClipLoader />
+          ) : (
+            <>
+              <h2 className="text-3xl whitespace-wrap">
+                Enter Reference Number below to check for application status
+              </h2>
+              <Input
+                type="text"
+                className="w-[250px] border border-neutral-500 rounded-md focus:outline-none"
+                placeholder="REFERENCE NUMBER"
+                value={refNum}
+                onChange={(e) => setRefNum(e.target.value)}
+              />
+              <Button
+                onClick={handleClick}
+                className="bg-cecOrange text-white text-center h-[40px] w-[40px] rounded-full hover:bg-[#8C6200] mb-12"
+              >
+                <Search size={15} />
+              </Button>
+              <BackButton backTo={"/"} />
+            </>
+          )
         ) : (
           <ReCAPTCHA
             onChange={handleCaptcha}
             sitekey="6Le90sQqAAAAAGR2kM-fSs__PTGEG5HjkM-w0piV"
           />
         )}
-        {/* <>
-          <h2 className="text-3xl whitespace-wrap">
-            Enter Reference Number below to check for application status
-          </h2>
-          <Input
-            type="text"
-            id=""
-            className="w-[250px] border border-neutral-500 rounded-md  focus:outline-none"
-            placeholder="REFERNCE NUMBER"
-            value={refNum}
-            onChange={(e) => {
-              setRefNum(e.target.value);
-            }}
-          />
-          <Button
-            onClick={handleClick}
-            className="bg-cecOrange text-white text-center h-[40px] w-[40px] rounded-full hover:bg-orange-700 mb-12"
-          >
-            <Search size={15} />
-          </Button>
-          <BackButton backTo={"/"} />
-        </> */}
       </div>
     </div>
   );
